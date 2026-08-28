f0f49ca03efe441e1650739d7b53a2ee8cdc5a11
Skullygxng <130867645+Skullygxng@users.noreply.github.com>
2026-08-28 18:29:08 -0400

    Export PPV iframe sandbox and referrer policy constants

---
+++ src/lib/ppvEmbedPolicy.ts
@@ -0,0 +1,3 @@
+export const PPV_IFRAME_SANDBOX = 'allow-scripts allow-presentation';
+export const PPV_IFRAME_REFERRER_POLICY = 'no-referrer';
+export const PPV_IFRAME_ALLOW = 'autoplay; fullscreen; encrypted-media; picture-in-picture';
